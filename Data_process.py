d5f419dac188bdf1af6207616d24ebcc2d09df41
cicolord <cicolord@outlook.com>
2018-03-26 00:37:52 +0800

    数据清洗完成，

--- Data_process.py
+++ Data_process.py
@@ -24,39 +24,63 @@ test2 = '''一
   【一体】关系密切，如同一个整体。一样，相同。
   【一昨】前些日子。
   '''
-
+#单词释义
 res_tr1 = r'【(.*?)】(.*)。'
-# print(test2)
+#扣关键字
 res_tr2 = r'([\u4e00-\u9fa5])\n'
+#扣解释，需要多行连在一起
 res_tr3 = r'(.*)”'
-data_1 = {}
+
+# data_1 = {}
 data_2 = {}
-# for lines in origin_file:
-#     return_str = ''
-#     single_word = re.findall(res_tr2,lines,re.S|re.M)
-#     pinyin = False
-#     dump_flag = False
-#     shiyi = re.findall(res_tr1, lines, re.S | re.M)
-#     if(not len(single_word) and not len(shiyi)):
-#         pinyin = True
-#     elif(len(list) and pinyin == False):
-#         m_tr = re.findall(res_tr3, lines, re.S | re.M)
-#         return_str = return_str + '\n' +m_tr
-#         print(return_str)
-#     elif(len(shiyi)):
-#         pinyin = False
-#         dump_flag = True
-#     elif(dump_flag==True):
-#         dump_flag = False
-#         data_2[single_word[0]] = return_str
-#         pickle.dump(data_2,output1)
-
-
+next_word = False
 for lines in origin_file:
-    m_tr = re.findall(res_tr1, lines, re.S | re.M)
-    for words in m_tr:
-        data_1[words[0]] = words[1]
-        # print(data_1)
-pickle.dump(data_1,output2)
-output2.close()
+    #temp是关键字
+    temp = re.findall(res_tr2, lines, re.S | re.M)
+    #temp2是单词释义
+    temp2 = re.findall(res_tr1, lines, re.S | re.M)
+    explanation = re.findall(res_tr3, lines, re.S | re.M)
+    if len(temp) == 1:
+        single_word = temp[0]
+        next_word = False
+        return_str = ''
+        continue
+    if next_word == False and len(explanation):
+        #explanation是解释
+        explanation = re.findall(res_tr3, lines, re.S | re.M)
+        # print(explanation)
+        return_str = return_str+'\n'+explanation[0]
+    if len(temp2):
+        next_word = True
+        data_2[single_word] = return_str
+        print(data_2)
+pickle.dump(data_2,output1)
+    # if one_word:
+    #     return_str = ''
+    #     single_word = re.findall(res_tr2,lines,re.S|re.M)
+    #     pinyin = False
+    #     dump_flag = False
+    #     shiyi = re.findall(res_tr1, lines, re.S | re.M)
+    #     if(not len(single_word) and not len(shiyi)):
+    #         pinyin = True
+    #     elif(len(list) and pinyin == False):
+    #         m_tr = re.findall(res_tr3, lines, re.S | re.M)
+    #         return_str = return_str + '\n' +m_tr
+    #         print(return_str)
+    #     elif(len(shiyi)):
+    #         pinyin = False
+    #         dump_flag = True
+    #     elif(dump_flag==True):
+    #         dump_flag = False
+    #         data_2[single_word[0]] = return_str
+    #         pickle.dump(data_2,output1)
+
+#扣释义的
+# for lines in origin_file:
+#     m_tr = re.findall(res_tr1, lines, re.S | re.M)
+#     for words in m_tr:
+#         data_1[words[0]] = words[1]
+#         # print(data_1)
+# pickle.dump(data_1,output2)
+# output2.close()
 output1.close()
