f6c2a9cbc1ce3bab3bc5d26be815b5cfe349b04a
cocolord <cocolord@users.noreply.github.com>
2018-04-12 17:02:15 +0800

    Add files via upload

--- 古文.py
+++ 古文.py
@@ -41,41 +41,23 @@ class CrawlData:
                         Ttag = Asoup.find(id=re.compile("leftbtn"))['id']
                         Ttag = Ttag[7:]
                         Map.writelines(str(i)+' '+Ttag+'\n')
+
+                        Tlink = "https://so.gushiwen.org/guwen/ajaxbfanyi.aspx?id=" + Ttag
+                        Treq = requests.get(Tlink, headers=headers).text
+                        Tsoup = BeautifulSoup(Treq, 'html.parser')
+                        Tsoup = Tsoup.get_text()
+                        # print(Tsoup)
+                        res_tr1 = r'全屏\n\n\n(.*)参考资料'
+                        Tsoup = re.findall(res_tr1, Tsoup, re.S | re.M)
+                        Tdest = 'Translation/' + 'trans'+str(i) + '.txt'
+                        if(Tsoup):
+                            with open(Tdest, 'w', encoding="utf-8") as Tfile:
+                                Tfile.writelines(Tsoup)
                     except:
                         pass
             except Exception :
                 error.writelines('Errors occur on page '+str(i) +'\n')
         Map.close()
-    def getPageFromYiWen(self):
-        for i in range(74,212):
-            try:
-                headers = {
-                    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-                    'Connection':'keep-alive',
-                    'Accept-Encoding':'gzip,deflate',
-                    'Accept-Language':'zh-CN;zh;q=0.9',
-                    'Cache-Control':'no-cache',
-                    'referer': 'https: // so.gushiwen.org / guwen / book_5.aspx',
-                    'Cookie':'ASP.NET_SessionId=4eldiva2okx4sqsz4yj2rohj; Hm_lvt_04660099568f561a75456483228a9516=1523426768; Hm_lpvt_04660099568f561a75456483228a9516=1523429161',
-                    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
-                }
-                Tdest = 'Translation/'+str(i)+'.txt'
-                with open(Tdest, 'w',encoding="utf-8") as Tfile:
-                    Tlink = "https://so.gushiwen.org/guwen/ajaxbfanyi.aspx?id="+str(i)
-                    Treq = requests.get(Tlink, headers=headers).text
-                    Tsoup = BeautifulSoup(Treq, 'html.parser')
-                    Tsoup = Tsoup.get_text()
-                    # print(Tsoup)
-                    res_tr1 = r'全屏\n\n\n(.*)参考资料'
-                    Tsoup = re.findall(res_tr1, Tsoup, re.S | re.M)
-                    # print(Tsoup)
-                    for line in Tsoup:
-                        Tfile.writelines(line)
-                    Tfile.writelines(Tsoup)
-            except Exception :
-                error.writelines('Errors occur on page '+str(i) +'\n')
-
 if __name__ == '__main__':
     one_Instance = CrawlData()
-    # one_Instance.getPageFromYiWen()
     one_Instance.getPageFromYuanwen()
